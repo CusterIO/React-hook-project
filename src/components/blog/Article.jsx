56b00ef44e8e9cc5f6db135677dbee91819c2e11
CusterIO <custer.app@gmail.com>
2019-04-13 17:14:52 +0200

    adjust validation and add material ui for css

--- src/components/blog/Article.jsx
+++ src/components/blog/Article.jsx
@@ -1,57 +1,124 @@
 import React, { useContext, useEffect } from "react";
 import { StateContext } from '../context/index';
-import { Slider } from '../sliders/Slider.jsx';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
 export const Article = () => {
   const {state, dispatch } = useContext(StateContext);
 
   useEffect(() => {
-    Validate(state.title);
+    const isTitleValid = Validate(state.title);
+    if (isTitleValid) {
+      dispatch({type: 'setTitleValidation', isTitleValid: true})
+    } else {
+      dispatch({type: 'setTitleValidation', isTitleValid: false})
+    }
   }, [state.title],
   );
 
+  useEffect(() => {
+    const isDescriptionValid = Validate(state.description);
+    if (isDescriptionValid) {
+      dispatch({type: 'setDescriptionValidation', isDescriptionValid: true})
+    } else {
+      dispatch({type: 'setDescriptionValidation', isDescriptionValid: false})
+    }
+  }, [state.description],
+  );
+
+  useEffect(() => {
+    const isAuthorValid = Validate(state.author);
+    if (isAuthorValid) {
+      dispatch({type: 'setAuthorValidation', isAuthorValid: true})
+    } else {
+      dispatch({type: 'setAuthorValidation', isAuthorValid: false})
+    }
+  }, [state.author],
+  );
+
+  useEffect(() => {
+    const isValid = ValidateAll();
+    if (isValid) {
+      dispatch({type: 'setValidation', isValid: true})
+    } else {
+      dispatch({type: 'setValidation', isValid: false})
+    }
+  }, [state.isAuthorValid, state.isDescriptionValid, state.isTitleValid],
+  ); 
+
+  const Validate = (input) => {
+    if (!input) {
+      return false;
+    }
+    else if (!input.match(/^[a-zA-Z0-9 .,!?)(\-\r\n]+$/)) {
+      dispatch({type: 'setValidationMsg', validationMsg: 'Only letters, numbers and characters .,!?)(- allowed'})
+      return false;
+    }
+    dispatch({type: 'setValidationMsg', validationMsg: ''})
+    return true;
+  };
+
+  const ValidateAll = () => {
+    return !!(state.isAuthorValid && state.isDescriptionValid && state.isTitleValid);
+  };
+
   return (
     <div className='article-component-wrapper'>
-      <input
-        className='article-component-title'
-        placeholder='Article title'
-        type='text'
-        value={state.title}
-        onChange={(e) => {
-          dispatch({type: 'setTitle', title: e.target.value})
-        }
-        }
-      />
-      <textarea
-        className='article-component-desciption'
-        placeholder='Article description'
-        value={state.description}
-        onChange={(e) => {
-          dispatch({type: 'setDescription', description: e.target.value})
-        }
-        }
-      />
-      <input
-        className='article-component-author'
-        placeholder='Article author'
-        type='text'
-        value={state.author}
-        onChange={(e) => {
-          dispatch({type: 'setAuthor', author: e.target.value})
-        }
-        }
-      />
-      <button
-        className='article-component-button'
-        onClick = {() => {
-          console.log('you clicked me');
-        }
-        }
-      />
+      <Typography variant="h6" gutterBottom>
+        {state.validationMsg}
+      </Typography>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <TextField
+            required
+            placeholder='Article title'
+            value={state.title}
+            onChange={(e) => {
+              dispatch({type: 'setTitle', title: e.target.value})
+            }
+            }
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            placeholder='Article description'
+            multiline={true}
+            rows={4}
+            rowsMax={40}
+            value={state.description}
+            onChange={(e) => {
+              dispatch({type: 'setDescription', description: e.target.value})
+            }
+            }
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            placeholder='Article author'
+            value={state.author}
+            onChange={(e) => {
+              dispatch({type: 'setAuthor', author: e.target.value})
+            }
+            }
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            color='primary'
+            variant='contained'
+            disabled={!state.isValid}
+            onClick = {() => {
+              console.log('you clicked me');
+            }
+            }
+          >
+          Post article
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
-};
-
-const Validate = (input) => {
-  console.log(input);
 };
--- src/components/context/index.jsx
+++ src/components/context/index.jsx
@@ -10,6 +10,16 @@ const reducer = (state, action) => {
       return { ...state, description: action.description };
     case "setAuthor":
       return { ...state, author: action.author };
+    case "setValidationMsg":
+      return { ...state, validationMsg: action.validationMsg };
+    case "setValidation":
+      return { ...state, isValid: action.isValid };
+    case "setTitleValidation":
+      return { ...state, isTitleValid: action.isTitleValid };
+    case "setDescriptionValidation":
+      return { ...state, isDescriptionValid: action.isDescriptionValid };
+    case "setAuthorValidation":
+      return { ...state, isAuthorValid: action.isAuthorValid };
     default:
       throw new Error("Unexpected action");
   }
@@ -19,7 +29,12 @@ const initialState = {
   slider: 0,
   title: "",
   description: "",
-  author: ""
+  author: "",
+  validationMsg: "",
+  isValid: false,
+  isTitleValid: false,
+  isDescriptionValid: false,
+  isAuthorValid: false
 };
 
 const StateContext = createContext(initialState);
